a02fdeea396929b7f12aef9960c1b0754b060344
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-03-08 07:00:41 +0330

    add update token prices.

--- app/Jobs/UpdatePriceJob.php
+++ app/Jobs/UpdatePriceJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Price;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -27,6 +28,28 @@ class UpdatePriceJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.osmosis.zone/tokens/v2/price/' + $this->token,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $data = json_decode($response, true);
+        Price::create([
+           'token' => $this->token,
+           'price' => $data['price'],
+           'change' => $data['change'],
+        ]);
+
     }
 }
--- app/Models/Price.php
+++ app/Models/Price.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Price extends Model
 {
     use HasFactory;
+    public $fillable = ['token', 'price', 'change'];
 }
