a66ba18f5e413a1a5fdba1486d5cb5087ebc1957
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-03-08 06:49:34 +0330

    add jobs.

---
+++ app/Jobs/UpdatePriceJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdatePriceJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $token;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
